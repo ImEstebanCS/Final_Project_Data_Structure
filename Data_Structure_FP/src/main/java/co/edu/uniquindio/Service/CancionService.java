7bc726b3fa831251e0e7e2d829b707e4dee8a036
nicolmarin <nicolmoreno090905@gmail.com>
2025-11-07 09:07:01 -0500

    usuario service y cancion service

--- Data_Structure_FP/src/main/java/co/edu/uniquindio/Service/CancionService.java
+++ Data_Structure_FP/src/main/java/co/edu/uniquindio/Service/CancionService.java
@@ -1,4 +1,110 @@
 package co.edu.uniquindio.Service;
 
+import java.util.Objects;
+
+
 public class CancionService {
+     private String id;
+    private String titulo;
+    private String artista;
+    private String genero;
+    private int año;
+    private int duracion; // en segundos
+
+    /**
+     * Constructor de la clase Cancion.
+     * 
+     * @param id Identificador único de la canción
+     * @param titulo Título de la canción
+     * @param artista Artista de la canción
+     * @param genero Género musical de la canción
+     * @param año Año de lanzamiento
+     * @param duracion Duración en segundos
+     */
+    public CancionService(String id, String titulo, String artista, String genero, int año, int duracion) {
+        this.id = id;
+        this.titulo = titulo;
+        this.artista = artista;
+        this.genero = genero;
+        this.año = año;
+        this.duracion = duracion;
+    }
+
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    /**
+     * RF-018: Implementa equals basado en el ID de la canción.
+     * 
+     * @param obj Objeto a comparar
+     * @return true si las canciones tienen el mismo ID
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        CancionService cancion = (CancionService) obj;
+        return Objects.equals(id, cancion.id);
+    }
+
+    /**
+     * RF-018: Implementa hashCode basado en el ID de la canción.
+     * 
+     * @return código hash basado en el ID
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s (%d) [%s]", artista, titulo, año, genero);
+    }
 }
--- Data_Structure_FP/src/main/java/co/edu/uniquindio/Service/UsuarioService.java
+++ Data_Structure_FP/src/main/java/co/edu/uniquindio/Service/UsuarioService.java
@@ -1,4 +1,111 @@
 package co.edu.uniquindio.Service;
 
+import java.util.LinkedList;
+import java.util.Objects;
+
+import co.edu.uniquindio.Model.Entidades.Cancion;
+import co.edu.uniquindio.Model.Entidades.Usuario;
+
 public class UsuarioService {
+    private String username;
+    private String password;
+    private String nombre;
+    private LinkedList<Cancion> cancionesFavoritas;
+
+    /**
+     * Constructor de la clase Usuario.
+     * 
+     * @param username Nombre de usuario único
+     * @param password Contraseña del usuario
+     * @param nombre Nombre completo del usuario
+     */
+    public UsuarioService(String username, String password, String nombre) {
+        this.username = username;
+        this.password = password;
+        this.nombre = nombre;
+        this.cancionesFavoritas = new LinkedList<>();
+    }
+
+    // Getters y Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LinkedList<Cancion> getCancionesFavoritas() {
+        return cancionesFavoritas;
+    }
+
+    public void setCancionesFavoritas(LinkedList<Cancion> cancionesFavoritas) {
+        this.cancionesFavoritas = cancionesFavoritas;
+    }
+
+    /**
+     * Agrega una canción a la lista de favoritos.
+     * 
+     * @param cancion Canción a agregar
+     */
+    public void agregarFavorita(Cancion cancion) {
+        if (!cancionesFavoritas.contains(cancion)) {
+            cancionesFavoritas.add(cancion);
+        }
+    }
+
+    /**
+     * Elimina una canción de la lista de favoritos.
+     * 
+     * @param cancion Canción a eliminar
+     */
+    public void eliminarFavorita(Cancion cancion) {
+        cancionesFavoritas.remove(cancion);
+    }
+
+    /**
+     * RF-015: Implementa equals basado en el username.
+     * 
+     * @param obj Objeto a comparar
+     * @return true si los usuarios tienen el mismo username
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        UsuarioService usuario = (UsuarioService) obj;
+        return Objects.equals(username, usuario.username);
+    }
+
+    /**
+     * RF-015: Implementa hashCode basado en el username.
+     * 
+     * @return código hash basado en el username
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Usuario{username='%s', nombre='%s', favoritas=%d}", 
+                username, nombre, cancionesFavoritas.size());
+    }
 }
